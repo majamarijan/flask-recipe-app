5b30abf3d6772ba6c0539093c4736a92ed9ed26d
majamarijan <majapianist@gmail.com>
2024-04-20 18:54:25 +0200

    add headers

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ config = dotenv_values(".env")
 # print(config.get("DOMAIN_URL"))
 CACHE_CONFIG = {
   "CACHE_TYPE": "SimpleCache",
-  "CACHE_DEFAULT_TIMEOUT": 120,
+  "CACHE_DEFAULT_TIMEOUT": 5,
   "DEBUG": True
 }
 # make application factory
@@ -28,7 +28,19 @@ def create_app():
   # instance_relative_config=True: load config from instance folder
   app = Flask(__name__, instance_relative_config=True)
   cache.init_app(app, config=CACHE_CONFIG)
-
+  with app.app_context():
+    # set headers
+    @app.after_request
+    def set_response_headers(response):
+      # register domain on https://hstspreload.org/ to prevent third party attacks and ensure https ; the list of domains that will browser download to use https
+      response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains; preload'
+      response.headers['Access-Control-Allow-Origin'] = 'same-origin'
+      response.headers['Content-Security-Policy'] = "default-src 'self' cdnjs.cloudflare.com; font-src 'self' fonts.gstatic.com fonts.googleapis.com cdnjs.cloudflare.com; style-src 'self' fonts.googleapis.com cdnjs.cloudflare.com unpkg.com 'unsafe-inline'; script-src 'self' cdnjs.cloudflare.com esm.sh"
+      response.headers['X-Content-Type-Options'] = 'nosniff'
+      response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+      response.headers['X-XSS-Protection'] = '1; mode=block'
+      response.headers['Referrer-Policy'] = 'strict-origin-when-cross-origin'
+      return response
   # add routes
   from .routes import public_views, post_views, api_views
  
--- app/routes/public_views.py
+++ app/routes/public_views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from . import public_route
 
 @public_route.get('/')
